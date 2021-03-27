b0fbd091524d0bdc80f2eb385f54659122ffb649
Nur Rafi <nurrafi@outlook.com>
2021-03-27 09:29:42 +0600

    Undo previous.

--- deleteproject.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('db.php');
-$project = $_GET['project'];
-$sql = "DELETE FROM project WHERE project_name='$project'";
-mysqli_query($con, $sql);
-header("Location: photographergallery.php");
