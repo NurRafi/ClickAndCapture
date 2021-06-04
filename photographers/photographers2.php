7b6036d9bd40a835c105b34b1e7119c74523a22e
mod.Monabbar Hossan Faisal <mhossan152026@bscse.uiu.ac.bd>
2021-06-05 02:42:25 +0600

    photographers part2

---
+++ photographers/photographers2.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login System</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<form name="registration" action="photographers.php" method="post">  
+<body>
+  
+    <section class="left-section">
+        <div id="left-cover" class="cover cover-hide">
+            <img src="img/boat.jpg" alt="">
+            <h1>Welcome !</h1>
+            <h3>Already have an account ?</h3>
+            <button type="button" class="switch-btn" onclick="location.reload()">Login</button>
+        </div>
+        <div id="left-form" class="form fade-in-element">
+            <h1>Login</h1>
+            <form action="login.php" method="post">
+			
+                <input type="text" name="email" class="input-box" placeholder="Enter your email">
+                <input type="password" name="password" class="input-box" placeholder="Enter your Password">
+                <input type="submit" name="login1" class="btn" value="Login">
+            </form>
+        </div>
+    </section>
+
+    <section class="right-section">
+        <div id="right-cover" class="cover fade-in-element">
+            <img src="img/cover.png" alt="">
+            <h1>Welcome !</h1>
+            <h3>Don't have an account ?</h3>
+            <button type="button" class="switch-btn" onclick="switchSignup()">Signup</button>
+        </div>
+        <div id="right-form" class="form form-hide">
+            <h1>Signup</h1>
+            <form action="photographers.php" method="post">
+			   <input type="id" name="user-id" class="input-box" placeholder="Photographer id">
+			     
+                <input type="text" name="user-name" class="input-box" placeholder="User Name">
+                <input type="email" name="email" class="input-box" placeholder="Email">
+                <input type="password" name="password" class="input-box" placeholder="Password">
+				<input type="Phone Num" name="phone" class="input-box" placeholder="Phone Num">
+				<input type="area" name="area" class="input-box" placeholder="Area">
+				<input type="city" name="city" class="input-box" placeholder="City">
+				<input type="price" name="price" class="input-box" placeholder="price">
+				<input type="Details Address" name="Address" class="input-box" placeholder="Details Address">
+				
+				<input type="submit" name="a" class="btn" value="Signup">
+            </form>
+        </div>
+    </section>
+
+    <script src="js/main.js"></script>
+
+</body>
+</form>
+</html>
+
+
