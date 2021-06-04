cc64e5804c7e9e887afc88df71fbe83ce97743c9
Nahian <nahianpathan12@gmail.com>
2021-06-05 02:35:46 +0600

    added php file to fetch data from data base and compare it to text box data

--- user.php
+++ user.php
@@ -91,3 +91,28 @@ VALUES ('$user_name','$email', '$password','$Mobile_Number')";
 	 
 	  
 ?>
+
+<?php
+require('db.php');
+// If form submitted, insert values into the database.
+if (isset($_POST['login1'])) {
+
+
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+    echo $email, $password;
+
+    $query = "SELECT * FROM user WHERE email='$email'
+      and password= '$password'";
+
+    $result = mysqli_query($con, $query) or die(mysql_error());
+
+    $rows = mysqli_num_rows($result);
+
+
+}
+	  
+	  
+    
+?>
