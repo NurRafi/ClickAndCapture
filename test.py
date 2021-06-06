de58d764f942edf62b40a8a3dc147407e619a18b
rafiarafia12 <rsulatana171240@bscse.uiu.ac.bd>
2021-06-06 16:33:48 +0600

    Testing signup.

--- test.py
+++ test.py
@@ -6,3 +6,19 @@ options = Options()
 options.headless = False
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.get("http://localhost/SE/")
+
+driver.find_element_by_id("photographer").click()
+
+driver.find_element_by_id("signup").click()
+
+driver.find_element_by_id("id_signup").send_keys("2")
+driver.find_element_by_id("username_signup").send_keys("Test")
+driver.find_element_by_id("email_signup").send_keys("test@gmail.com")
+driver.find_element_by_id("password_signup").send_keys("123")
+driver.find_element_by_id("phone_signup").send_keys("123")
+driver.find_element_by_id("area_signup").send_keys("Mohammadpur")
+driver.find_element_by_id("city_signup").send_keys("Dhaka")
+driver.find_element_by_id("price_signup").send_keys("2500")
+driver.find_element_by_id("address_signup").send_keys("Ring Road")
+
+driver.find_element_by_id("submit_signup").click()
