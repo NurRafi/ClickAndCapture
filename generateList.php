b25a3ff5019cfe1e9efafc885061f52b8565795b
rafiarafia12 <rsulatana171240@bscse.uiu.ac.bd>
2021-06-06 16:26:27 +0600

    Writing to file.

--- generateList.php
+++ generateList.php
@@ -4,3 +4,5 @@ $sql = "select name, email from user";
 $result = mysqli_query($connection, $sql);
 $row = mysqli_fetch_assoc($result);
 $result = $connection->query($sql);
+
+$xmlFile = fopen("userList.xml", "w") or die("Unable to open file!");
