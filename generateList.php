1aac40d03ddeea5b23ae7fd7bf453e5bfcc3077a
rafiarafia12 <rsulatana171240@bscse.uiu.ac.bd>
2021-06-06 16:26:56 +0600

    Writing some texts.

--- generateList.php
+++ generateList.php
@@ -6,3 +6,9 @@ $row = mysqli_fetch_assoc($result);
 $result = $connection->query($sql);
 
 $xmlFile = fopen("userList.xml", "w") or die("Unable to open file!");
+
+$text = '<xml>';
+fwrite($xmlFile, $text);
+
+$text = "<pages>";
+fwrite($xmlFile, $text);
