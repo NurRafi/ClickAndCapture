9dc6c8e987780b1427809bb4359b60a6bc12038f
mstanzumanabegum <asha.anzum78@gmail.com>
2021-06-06 15:20:01 +0600

    I am going to take the three images from the database.

--- deleteproject.php
+++ deleteproject.php
@@ -5,3 +5,7 @@ $project = $_GET['project'];
 $sql = "select image, image1, image2 from project where project_name = '$project'";
 $result = mysqli_query($con, $sql);
 $row = mysqli_fetch_assoc($result);
+
+$image = $row['image'];
+$image1 = $row['image1'];
+$image2 = $row['image2'];
