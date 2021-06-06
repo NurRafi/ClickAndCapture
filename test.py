56fae43d1756aec7c9b7275ef7ed0374e43a7b55
rafiarafia12 <rsulatana171240@bscse.uiu.ac.bd>
2021-06-06 16:33:23 +0600

    Added the option for headless so that the testing can be done fast!

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.headless = False
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver.get("http://localhost/SE/")
