ee6ccc76764447a0a558068d6ca8abbfa3d7e5ef
rafiarafia12 <rsulatana171240@bscse.uiu.ac.bd>
2021-06-06 16:35:00 +0600

    Fixing a mistake.

--- test.py
+++ test.py
@@ -18,7 +18,5 @@ driver.find_element_by_id("password_signup").send_keys("123")
 driver.find_element_by_id("phone_signup").send_keys("123")
 driver.find_element_by_id("area_signup").send_keys("Mohammadpur")
 driver.find_element_by_id("city_signup").send_keys("Dhaka")
-driver.find_element_by_id("price_signup").send_keys("2500")
-driver.find_element_by_id("address_signup").send_keys("Ring Road")
 
 driver.find_element_by_id("submit_signup").click()
